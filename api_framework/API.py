cbdb62c9cd56167538d0be2b42bdac07409ecf0a
cyberbeast <sandeshgade@gmail.com>
2016-02-21 13:10:45 +0530

    LATEST AMAZON CLOUD DEPLOY

--- api_framework/API.py
+++ api_framework/API.py
@@ -3,24 +3,22 @@ from PIL import Image, ImageDraw
 
 @hug.get(output=hug.output_format.mp4_video, versions=1)
 def watch():
-    """Watch an example movie, streamed directly to you from pinkhelium"""
+    """Watch an example movie, streamed directly to you from pinkhelium demonstrating the power of our api platform"""
     return 'movie.mp4'
 
 @hug.get('/watch2',output=hug.output_format.mp4_video, versions=2)
 def watch():
-    """Watch an example movie, streamed directly to you from pinkhelium"""
+    """Watch an example movie, streamed directly to you from pinkhelium. This is version 2 of the popular watch api."""
     return 'movie.mp4'
 
 @hug.get('/image', output=hug.output_format.png_image)
 def create_image(text):
-    image = Image.new('RGB', (500, 500)) # create the image
+    image = Image.new('RGB', (500, 500))
     ImageDraw.Draw(image).text((100, 100), text, fill=(255, 0, 0))
     return image
 	
 
 @hug.get('/greet_user', versions=1)
-
-
 def greet_user(values):
 	'''Describe Your Function Here!'''
 	return("Welcome to pinkhelium" + values)
@@ -28,18 +26,22 @@ def greet_user(values):
 	
 
 @hug.get('/dummy', versions=4)
-
-
 def dummy(values):
 	'''Describe Your Function Here!'''
 	return(values)
 	
 
 @hug.get('/deploy', versions=3)
-
-
 def deploy(values):
 	'''deploy function is this!'''
 	return("DEPLOYED")
+	
+
+@hug.get('/deploy2', versions=4)
+
+
+def deploy2(values):
+	'''Describe Your Function Here!'''
+	return("Hello World")
 	
 
---
+++ api_framework/deploy2.py
@@ -0,0 +1,6 @@
+
+
+def deploy2(values):
+	'''Describe Your Function Here!'''
+	return("Hello World")
+	
